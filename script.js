56ca9d82d14d006025895f63ca4965d43581c32e
Skion6 <ozwemer@gmail.com>
2025-04-09 19:20:47 -0700

    Update script.js
    sdsdsds

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function checkInput2() {
     let input = document.getElementById("pw2").value;
 
     const response = await fetch("https://api.jsonbin.io/v3/b/67f3f7068960c979a57ff95b/latest", {
-        headers: { "X-Access-Key": "$2a$10$NJ/HfLsUKJHwVndIHPD7beGOnwlLvavpq9HZMUVfmrbMAzsvAVBUG" }  // Use your actual secret key here
+        headers: { "X-Access-Key": "SECRETPASS" }  // Use your actual secret key here
     });
 
     const data = await response.json();
